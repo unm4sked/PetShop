7c81301ae637fe6bff41ee24d8ae185f57931b1c
Lukasz Kalicki <lukasz.k.kalicki@gmail.com>
2018-01-12 22:11:06 +0100

    develop dontracts

---
+++ test/TestAdoption.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.17;
+
+import "truffle/Assert.sol";
+import "truffle/DeplyedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdopton {
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+    function testUserCanAdopePet() public {
+        uint returnedId = adoption.adopt(8);
+        uint expected = 8;
+        Assert.equal(returnedId,expected,"Adoption of pet ID 8 should be recorded.");
+    }
+
+    function testGetAdopterAddressByPetId() public {
+
+        address expected = this;
+        address adopter = adoption.adopters(8);
+
+        Assert.equal(adopter, expected, "Owner of pet ID should by recorded");
+    }
+
+    function testGetAdopterAddressByPetIdInArray() public {
+
+        address expected = this;
+
+        address[16] memory adopters = adoption.getAdopters();
+
+        Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.")
+    }
+
+}
+
